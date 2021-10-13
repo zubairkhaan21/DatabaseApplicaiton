4e0aad8649bfae8898ce6b073e2aea5f347a017c
Muhammad Zubair <zubairkhaan21@gmail.com>
2021-10-13 12:45:56 +0200

    Add project files.

---
+++ DatabaseApplicaiton/SignUp.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace DatabaseApplicaiton
+{
+    partial class SignUp
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(141, 68);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(73, 20);
+            this.username.TabIndex = 0;
+            this.username.Text = "username";
+            this.username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 240);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Store DAta ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(258, 115);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(286, 27);
+            this.textPassword.TabIndex = 4;
+            this.textPassword.Text = "Sign Up";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(258, 61);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(286, 27);
+            this.textEmail.TabIndex = 3;
+            this.textEmail.Text = "Login";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username);
+            this.Name = "SignUp";
+            this.Text = "Sign Up ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DatabaseApplicaiton/SignUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseApplicaiton
+{
+    public partial class SignUp : Form
+
+    {
+        DBAccess dBAccess = new DBAccess();
+        DataTable dataTable = new DataTable();
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = textEmail.Text;
+            string passwrod = textPassword.Text;
+
+            SqlCommand insrtQuery = new SqlCommand("insert into Users(Username,Password) values (@userName,@passwrod)");
+            insrtQuery.Parameters.AddWithValue("@userName", userName);
+            insrtQuery.Parameters.AddWithValue("@passwrod", passwrod);
+
+            int row = dBAccess.executeQuery(insrtQuery);
+
+            if (row == 1) {
+                MessageBox.Show("Account Created Successfully");
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = textEmail.Text;
+            string password = textPassword.Text;
+
+            string query = "select * from Users where Username='"+username+"'AND Password = '"+password+"'";
+            dBAccess.readDatathroughAdapter(query,dataTable);
+
+            if(dataTable.Rows.Count == 1) {
+                MessageBox.Show("  Successfully");
+
+                dBAccess.closeConn();
+
+            }
+        }
+    }
+}
